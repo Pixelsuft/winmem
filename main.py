d2d48ad41e0fd30b1dcf2d16806272ea53f62c5b
pixelsuft <yuskivl07@mail.ru>
2021-08-17 08:51:28 +0700

    updates

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ memory = winmem.get_memory(
         val = f'{val} {hex(val)}'
     print(i, val)'''  # Show all values and functions in "Memory" class
 
-print('cocos base address', hex(memory.get_base_address('libcocos2D.dll')))
+print('cocos base address', hex(memory.get_base_address('libcocos2D.dll')))  # Find base address of libcocos2d.dll
 memory.inject_dll(r"D:\Program Files\Geometry Dash\adaf-dll1\GDLocalisation.dll")  # Inject dll
 print('is_dead boolean address', memory.resolve_layers(0x3222D0, 0x164, 0x39C))  # Get address by pointer
 print('uint32 value', memory.read_uint32(memory.base_address + 0x2CDF44))  # Read uint32 value
@@ -24,5 +24,5 @@ print('nickname string', memory.read_string(memory.get_pointer_address(0x3222D8,
 memory.write_string('Pixelsufted', memory.get_pointer_address(0x3222D8, 0x108))  # Write string
 
 
-# time.sleep(5)
-# memory.terminate(1337)
+# time.sleep(5)  # Sleep 5 seconds
+# memory.terminate(1337)  # Terminate app with exit code 1337
